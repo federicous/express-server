a5c9608eeb48a05bbd2340d0faa7ccffcefc7ad6
Fede <federicous@gmail.com>
2021-12-17 00:31:46 -0300

    cart delete producto

--- views/ejs/ejs.js
+++ views/ejs/ejs.js
@@ -7,13 +7,10 @@ const router = Router()
 const routerProd = Router()
 const routerCart = Router()
 
-// carga del modulo Handlebars
-// const handlebars = require("express-handlebars")
-// const pug= require("pug")
+// carga del modulo EJS
 const ejs= require("ejs")
 
 app.set("view engine", "ejs");
-// app.set("views","./views/ejs")
 
 let misProductos = new Contenedor('./public/productos.txt')
 let misCarritos = new Contenedor('./public/carritos.txt')
@@ -27,21 +24,11 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(express.static("./public"));
 
-/* 
-app.get('/', (req, res) => {
-	// res.send({ mensaje: 'hola universo' })
-   // res.send("index", {});
-   res.sendFile(__dirname + '/public/index.html');
-     })
- */
-
-     app.get('/', async (req, res) => {
-      // res.send({ mensaje: 'hola universo' })
-      // res.send("index", {});
-      let total= await misProductos.getAll();
-      res.render('ejs/pages',{productList: total, listExist: total.length, productTable: false});
-      // res.render('ejs/pages',{mensaje: 'usando pug'})
-        })
+app.get('/', async (req, res) => {
+   let total= await misProductos.getAll();
+   res.render('ejs/pages',{productList: total, listExist: total.length, productTable: false});
+   // res.render('ejs/pages',{mensaje: 'usando pug'})
+   })
 
 app.post('/uploadproduct', async function (req, res) {
       console.log("post recibido");
@@ -72,21 +59,20 @@ app.get('/productoRandom', async (req, res) => {
    res.json(resultado)
 })
 
-
-
 /* ############################## Product ###################################### */
+// ----------- GET ---------------
 routerProd.get('/', async (req, res, next) => {
    let total= await misProductos.getAll();
    res.json(total)
 })
-
+// ----------- GET ID---------------
 routerProd.get('/:id', async (req, res) => {
    console.log(req.params.id);
    let resultado= await misProductos.getById(parseInt(req.params.id))
    console.log(resultado);
       res.json(resultado)
 })
-
+// ----------- POST ---------------
 routerProd.post('/', async (req, res, next) => {
    console.log(req.body)
    let resultado= await misProductos.save(req.body)
@@ -94,7 +80,7 @@ routerProd.post('/', async (req, res, next) => {
    let total= await misProductos.getAll();
    res.json(total)
 })
-
+// ----------- PUT ---------------
 routerProd.put('/:id', async (req, res, next) => {
    await misProductos.deleteById(req.params.id);
    await misProductos.save(req.body, req.params.id)
@@ -104,7 +90,7 @@ routerProd.put('/:id', async (req, res, next) => {
       new: req.body
    })
 })
-
+// ----------- DELETE ---------------
 routerProd.delete('/:id', async (req, res, next) => {
    console.log(req.params.id);
    await misProductos.deleteById(req.params.id);
@@ -113,30 +99,31 @@ routerProd.delete('/:id', async (req, res, next) => {
       id: req.params.id      
    })
 })
-
 /* ############################## Fin Product ###################################### */
 
 /* ############################## Cart ###################################### */
+// ----------- GET ---------------
 routerCart.get('/', async (req, res, next) => {
    let total= await misCarritos.getAll();
    res.json(total)
 })
-
+// ----------- GET ID ---------------
 routerCart.get('/:id', async (req, res) => {
    console.log(req.params.id);
    let resultado= await misCarritos.getById(parseInt(req.params.id))
    console.log(resultado);
       res.json(resultado)
 })
-
+// ----------- POST ---------------
 routerCart.post('/', async (req, res, next) => {
    console.log(req.body)
    let resultado= await misCarritos.save(req.body)
 
    let total= await misCarritos.getAll();
-   res.json(total)
+   res.json({id: total[total.length - 1].id})
+   console.log(`Id del pedido: ${total[total.length - 1].id}`);
 })
-
+// ----------- PUT ---------------
 routerCart.put('/:id', async (req, res, next) => {
    await misCarritos.deleteById(req.params.id);
    await misCarritos.save(req.body, req.params.id)
@@ -146,7 +133,7 @@ routerCart.put('/:id', async (req, res, next) => {
       new: req.body
    })
 })
-
+// ----------- DELETE Cart---------------
 routerCart.delete('/:id', async (req, res, next) => {
    console.log(req.params.id);
    await misCarritos.deleteById(req.params.id);
@@ -155,10 +142,31 @@ routerCart.delete('/:id', async (req, res, next) => {
       id: req.params.id      
    })
 })
+// ----------- DELETE Product from Cart---------------
+routerCart.delete('/:id/product/:id_prod', async (req, res, next) => {
+   // console.log(`id cart: ${req.params.id}`);
+   // console.log(`id producto: ${req.params.id_prod}`);
+   let cart= await misCarritos.getById(parseInt(req.params.id))
+   // console.log(`cart a modificar: ${JSON.stringify(cart)}`);
+   let aux=cart
+   // console.log(`aux.cartList: ${JSON.stringify(aux.cartList)}`);
+   // console.log(`aux.cartList[${req.params.id_prod}]: ${JSON.stringify(aux.cartList[req.params.id_prod])}`);
+   let indice=aux.cartList.findIndex(product=>product.id===parseInt(req.params.id_prod))
+   // console.log(`indice borrar: ${indice}`);
 
+   aux.cartList.splice(indice,1)
+   // console.log(`nuevo carrito : ${JSON.stringify(aux)}`);
+   // borro el carrito original
+   await misCarritos.deleteById(req.params.id);
+   // guardo el carrito modificado
+   await misCarritos.save(aux, req.params.id)
+
+   res.json({
+      result:'ok',
+      id: req.params.id      
+   })
+})
 /* ############################## Fin Cart ###################################### */
-
-
 
 app.use('/api', router)
 app.use('/api/product', routerProd)
@@ -167,11 +175,3 @@ app.use('/api/cart', routerCart)
 app.listen(PORT, () => {
    console.log(`Servidor http escuchando en http://localhost:${PORT}`)
 })
-
-
-
-/* const server = app.listen(PORT, () => {
-   console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-})
-
-server.on("error", error => console.log(`Error en servidor ${error}`)) */
